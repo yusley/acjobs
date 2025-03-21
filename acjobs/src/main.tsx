b81e15f4cdb9980d84236d77ef4799da6456da0f
yusley <yusley.santos23@gmail.com>
2025-03-21 11:32:28 -0500

    adicao das rotas

--- acjobs/src/main.tsx
+++ acjobs/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './pages/Home'
+import Routers from './routes'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Home />
+      <Routers />
     </BrowserRouter>
   </StrictMode>,
 )
--- acjobs/src/routes/index.tsx
+++ acjobs/src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { Routes,Route,Navigate } from "react-router";
 import Home from "../pages/Home";
-
+import Login from "../pages/Login";
 
 function Routers(){
     return(
         <Routes>
             
             <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
             
             
         </Routes>
     )
 }
+
+export default Routers;
