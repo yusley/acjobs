42dda1d64f92de5815d31a3e2c724a1c45281fbe
yusley <yusley.santos23@gmail.com>
2025-03-21 22:23:55 -0300

    homepage cards e header

--- acjobs/src/components/CardJob/index.tsx
+++ acjobs/src/components/CardJob/index.tsx
@@ -1,9 +1,11 @@
 import './styles.css'
 import { IoLocationOutline } from 'react-icons/io5';
+import { PiCityDuotone } from "react-icons/pi";
+
 
 function CardJob ({...props}) {
     return(
-        <div className="w-full max-w-[45%] flex flex-col shadow-xl font-sans bg-[#fff] p-[1rem] text-3xl rounded-2xl gap-3">
+        <div className="w-full xl:w-[45%] md:w-[45%] flex flex-col shadow-xl font-sans bg-[#fff] p-[1rem] text-3xl rounded-2xl gap-3">
             <div className="title">
                 <h3 className="">Analista de Dados</h3>
             </div>
@@ -12,18 +14,23 @@ function CardJob ({...props}) {
                     Presencial
                 </p>
             </div>
-            <div className="location py-1 flex items-center">
+            <div className="location py-1 flex items-center gap-1">
                 <IoLocationOutline/>
                 <p className='text-[1rem] font-semibold text-slate-600'>
                     Rio Branco - AC
                 </p>
             </div>
             <div className="details w-full flex justify-between mt-10">
-                <div className="interpraise">
-                    SONDA
+                <div className="interpraise py-1 flex items-end gap-1">
+                    <PiCityDuotone />
+                    <p className='text-[1rem] font-semibold text-slate-600'>
+                        SONDA
+                    </p>
                 </div>
                 <div className="detailsLink flex ">
-                    <button className='bg-green-500 text-[#fff] text-[18px] p-3 cursor-pointer'>Detalhes</button>
+                    <a href="#">
+                        <button className='bg-green-500 text-[#fff] text-[18px] p-3 cursor-pointer'>Detalhes</button>
+                    </a>
                 </div>
             </div>
         </div>
